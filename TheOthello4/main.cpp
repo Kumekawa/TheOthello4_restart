def886808deb672330e4dc3b239cd285d5a480b2
Kumekawa <al17042@shibaura-it.ac.jp>
2019-08-02 21:59:45 +0900

    盤面の描画処理を作成

--- TheOthello4/main.cpp
+++ TheOthello4/main.cpp
@@ -3,6 +3,9 @@
 #include "DxLibAlways.h"
 #include "DxLibEndEffect.h"
 #include "Between.h"
+#include "DrawLineStright.h"
+#include <vector>
+using namespace std;
 
 constexpr int  MFS_XSIZE = 8;
 constexpr int  MFS_YSIZE = 8;
@@ -26,6 +29,17 @@ public:
 class Field :public BaseClass {
 	eFieldColor stone[MFS_XSIZE][MFS_YSIZE];
 	eFieldColor *turnPlayer;
+	void DrawStone(int x, int y,eFieldColor efc) {
+		int i = x * MFS_UNIT + MFS_UNIT / 2;
+		int j = y * MFS_UNIT + MFS_UNIT / 2;
+		int c = MC_BLACK;
+		if (stone[x][y] != eFC_None) {
+			if (stone[x][y] == eFC_White) {
+				c = MC_WHITE;
+			}
+			DrawCircle(i, j, MFS_UNIT / 3, c, 1);
+		}
+	}
 public:
 	void Initialize()override {
 		//”Õ–ÊƒŠƒZƒbƒg
@@ -34,14 +48,27 @@ public:
 				stone[i][j] = eFC_None;
 			}
 		}
-		stone[MFS_XSIZE - 1][MFS_XSIZE - 1] = eFC_Black;
-		stone[MFS_XSIZE][MFS_XSIZE] = eFC_Black;
-		stone[MFS_XSIZE - 1][MFS_XSIZE] = eFC_White;
-		stone[MFS_XSIZE][MFS_XSIZE - 1] = eFC_White;
+		int tx = MFS_XSIZE / 2;
+		int ty = MFS_YSIZE / 2;
+		stone[tx - 1][ty - 1] = eFC_Black;
+		stone[tx][ty] = eFC_Black;
+		stone[tx - 1][ty] = eFC_White;
+		stone[tx][ty - 1] = eFC_White;
 	};
 	void Update()override {};
 	void Draw()override {
-		
+		DrawBox(0, 0, MFS_WIDTH, MFS_HEIGHT, MC_GREEN, 1);
+		for (int i = 0; i <= MFS_XSIZE; ++i) {
+			DrawLineVertical(i * MFS_UNIT, MC_BLACK);
+		}
+		for (int j = 0; j <= MFS_YSIZE; ++j) {
+			DrawLineHorizontal(j * MFS_UNIT, MC_BLACK);
+		}
+		for (int i = 0; i < MFS_XSIZE; ++i) {
+			for (int j = 0; j < MFS_YSIZE; ++j) {
+				DrawStone(i, j, stone[i][j]);
+			}
+		}
 	};
 
 	void SetStone(int x,int y) {
@@ -86,9 +113,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	//•`‰æ‰æ–Ê‚ð— ‚É
 	SetDrawScreen(DX_SCREEN_BACK);
 
+	vector<BaseClass*> objects;
+	Field field;
+	objects.push_back(&field);
+
+	for (int i = 0; i < objects.size(); ++i) {
+		objects[i]->Initialize();
+	}
+
 	while (ProcessMessage() == 0)
 	{
-
+		for (int i = 0; i < objects.size(); ++i) {
+			objects[i]->Update();
+			objects[i]->Draw();
+		}
 		if (DxLibEndEffect(true)) {
 			break;
 		}
