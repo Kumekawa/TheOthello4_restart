2e6efe5a214d510bd2e0735ad1160c97c9ca3f6d
Kumekawa <al17042@shibaura-it.ac.jp>
2019-08-03 19:59:04 +0900

    裏でランダムに100回やるやつを追加

--- TheOthello4/main.cpp
+++ TheOthello4/main.cpp
@@ -616,6 +616,54 @@ public:
 	}
 };
 
+//置ける場所に置いたとき、お互いがランダムに置きあうのを1000回繰り返し、勝率が高いところを選ぶ
+class PlayerRandomHyper :public BasePlayer {
+	bool SetPosition() override {
+		//仮想環境を作り、自分の手番が最大になるように選ぶ
+		eFieldColor _turnPlayer = myColor;
+		Field _field(&_turnPlayer);
+		int max = -1;
+
+		_field.SetFieldStone(field->GetFieldStone());
+		for (int i = 0; i < _field.GetNextStones().size(); ++i) {
+			//終了するまで殴り合い
+			int t = 0;
+			auto ft = _field;
+			_turnPlayer = myColor;
+
+			int tx = ft.GetNextStones()[i].x;
+			int ty = ft.GetNextStones()[i].y;
+
+			for (int i = 0; i < 100; ++i) {
+				ft = _field;
+				_turnPlayer = myColor;
+
+				//ここで初回の石配置
+				ft.SetStone(tx, ty);
+				PlayerRandom Player2(&ft, &_turnPlayer, GetChangeFieldColor(myColor), false);
+				PlayerRandom Player1(&ft, &_turnPlayer, myColor, false);
+
+				while (ft.GetEndF() == 0) {
+					ft.Update();
+					Player2.Update();
+					Player1.Update();
+				}
+
+			}
+			if (max < t) {
+				max = t;
+				fx = tx;
+				fy = ty;
+			}
+		}
+		return true;
+	}
+public:
+	PlayerRandomHyper(Field *field, eFieldColor *turnPlayer, eFieldColor myColor, bool saveF = true) :BasePlayer(field, turnPlayer, myColor, saveF) {
+
+	}
+};
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	SetOutApplicationLogValidFlag(false);//ログを出力しない
@@ -659,8 +707,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	//PlayerNextMax player2(&field, &turnPlayer, eFC_White, saveF);
 	//PlayerMinMax player2(&field, &turnPlayer, eFC_White, saveF);
 	//PlayerMyAlgorithm player2(&field, &turnPlayer, eFC_White, saveF);
-	PlayerMinMaxHyper player2(&field, &turnPlayer, eFC_White, saveF);
-	
+	//PlayerMinMaxHyper player2(&field, &turnPlayer, eFC_White, saveF);
+	PlayerRandomHyper player2(&field, &turnPlayer, eFC_White, saveF);
+
 	objects.push_back(&player1);
 	objects.push_back(&player2);
 
